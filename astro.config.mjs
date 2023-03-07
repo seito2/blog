00e5bc63732d4e9de97174214bb093f8c1d645cb
seito2 <seito1201@gmail.com>
2023-03-07 14:44:34 +0900

    Add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://seito2.github.io/blog/",
   integrations: [mdx(), sitemap()],
   outDir: "dist",
+  base: "/blog",
 });
